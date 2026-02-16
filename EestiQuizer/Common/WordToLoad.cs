88a6cc7a9fa1693d38143f79d8afa3e87eefffa4
Dominik <kastalor@gmail.com>
2026-02-16 18:42:18 +0100

    Forgot to also move WordToLoad into Common part.

--- EestiQuizer/Common/WordToLoad.cs
+++ EestiQuizer/Common/WordToLoad.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EestiQuizer; 
+namespace EestiQuizer.Common; 
 
 
 // Since I am collecting the words from each chapter, I am already saving them into files with the chapter name in it.
